84f0ca6dd23bb22a4461754786cf125022df6b0e
Ugur <32577192+ugurgog@users.noreply.github.com>
2020-01-31 17:03:35 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -24,29 +24,54 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
 // https://us-central1-colorful-intelligence.cloudfunctions.net/addQuestion?question=xxx&answer=xxxx&id=2
 exports.addQuestion = functions.https.onRequest(async (req, res) => {
     console.log("addQuestion******************************" );
-    const question = req.query.question;
-    const answer = req.query.answer;
-    const quesId = req.query.id;
+    
+    try {
+		const question = req.query.question;
+	    const answer = req.query.answer;
+	    const quesId = req.query.id;
 
-    console.log("question:" , question);
-    console.log("answer  :" , answer);
-    console.log("quesId  :" , quesId);
+	    console.log("question:" , question);
+	    console.log("answer  :" , answer);
+	    console.log("quesId  :" , quesId);
 
-    const ques = {
-    	"answer" : answer,
-    	"question" : question
-  	} 
+	    
+	    //getTotalQuestionCount();
+	    
+	    console.log("quesCount  :" , quesCount);
 
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    const snapshot = await admin.database().ref('/QUES/' + quesId + '/').set(ques);
+	    const ques = {
+	    	"answer" : answer,
+	    	"question" : question
+	  	} 
 
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref.toString());
+	    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+	    const snapshot = await admin.database().ref('/QUES/' + quesId + '/').set(ques);
+
+	    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+	    //res.redirect(303, snapshot.ref.toString());
+
+	    return res.status(200).send('Question insert ok');
+  } catch (error) {
+  		/*var retVal = "error" :{
+  			"status" : 400,
+  			"req" : req,
+  			"message": error.message
+  		}*/
+  		console.log("req body  :", req.body)
+  		console.log("req query :", req.query)
+  		console.log("req params:", req.params)
+    	return res.status(400).send(req.body + " - " + error.message);
+  }
 });
 
 
-/*exports.addQuestionNew = functions.database.ref('/QUES/{id}/{query}').onWrite(event => {
+/*function getTotalQuestionCount() {
+	var ref = firebase.database().ref('QUES/');
 
-	event.data.ref.parent.child()
-});*/
+	ref.on('value', function(snapshot) {
+  		console.log("snapshot.val():", snapshot.val());
+	});
+}*/
+
+
 
